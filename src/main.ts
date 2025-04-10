26fc098973c04526b6938610890e1d8ec268ab89
MarcioBittencourt <marciobitt97@gmail.com>
2025-04-10 14:14:56 -0300

    Adicionado configurações extras

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  
+  const config = new DocumentBuilder()
+    .setTitle('Reconhecimento API')
+    .setDescription('Documentação da API com Swagger')
+    .setVersion('1.0')
+    .addTag('reconhecimentos')
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
+
 }
 bootstrap();
