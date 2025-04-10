898fa5dd14a22709a6daf93fab3e2ebe11b3eefd
MarcioBittencourt <marciobitt97@gmail.com>
2025-04-10 15:14:11 -0300

    Configuração do typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReconhecimentoModule } from './reconhecimentos/reconhecimento.module';
+import { Reconhecimento } from './reconhecimentos/entities/reconhecimento';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ReconhecimentoModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [Reconhecimento],
+      synchronize: true,
+    }),
+    ReconhecimentoModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
