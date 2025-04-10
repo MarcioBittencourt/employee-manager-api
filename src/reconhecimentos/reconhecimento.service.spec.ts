ca90ac96b39e69c4dccf51e88ae1fcd023543979
MarcioBittencourt <marciobitt97@gmail.com>
2025-04-10 16:27:53 -0300

    Refatoração dos testes usando array in-memory para TypeOrm e SQLite

--- src/reconhecimentos/reconhecimento.service.spec.ts
+++ src/reconhecimentos/reconhecimento.service.spec.ts
@@ -1,75 +1,72 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReconhecimentoService } from './reconhecimento.service';
-import { ReconhecimentoRepository } from './reconhecimento.repository';
+import { Repository } from 'typeorm';
+import { Reconhecimento } from './entities/reconhecimento';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('ReconhecimentoService', () => {
   let service: ReconhecimentoService;
+  let repo: Repository<Reconhecimento>;
+
+  const mockReconhecimento = {
+    "id": 1,
+    "from": "Beltrano",
+    "to": "Ciclano",
+    "message": "Excelente trabalho na entrega do projeto, sua dedicação fez toda a diferença!",
+    "coins": 10
+  };
+
+  const mockRepository = {
+    create: jest.fn().mockImplementation(dto => dto),
+    save: jest.fn().mockResolvedValue(mockReconhecimento),
+    find: jest.fn().mockResolvedValue([mockReconhecimento]),
+    findOneByOrFail: jest.fn().mockResolvedValue(mockReconhecimento),
+    remove: jest.fn().mockResolvedValue(mockReconhecimento),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ReconhecimentoService, ReconhecimentoRepository],
+      providers: [ReconhecimentoService,
+        {
+          provide: getRepositoryToken(Reconhecimento),
+          useValue: mockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<ReconhecimentoService>(ReconhecimentoService);
+    repo = module.get<Repository<Reconhecimento>>(getRepositoryToken(Reconhecimento));
   });
 
   it('deve cadastrar um reconhecimento', async () => {
-
     const reconhecimento = {
-      "uuid": "3",
-      "id": 3,
+      "id": 1,
       "from": "Beltrano",
       "to": "Ciclano",
       "message": "Excelente trabalho na entrega do projeto, sua dedicação fez toda a diferença!",
-      "coins": 1
+      "coins": 10
     };
-
     const resultado = await service.create(reconhecimento);
-
-    expect(resultado).toHaveProperty('id');
-    expect(resultado).toHaveProperty('uuid');
-    expect(resultado.from).toBe('Beltrano');
-    expect(resultado.to).toBe('Ciclano');
-    expect(resultado.message).toBe('Excelente trabalho na entrega do projeto, sua dedicação fez toda a diferença!');
-    expect(resultado.coins).toBe(1);
+    expect(resultado).toEqual(mockReconhecimento);
+    expect(repo.create).toHaveBeenCalledWith(reconhecimento);
+    expect(repo.save).toHaveBeenCalledWith(mockReconhecimento);
   });
-
+  
   it('deve retornar um array com todos os reconhecimentos', async () => {
-
     const resultado = await service.findAll();
-
-    expect(Array.isArray(resultado)).toBe(true);
-    expect(resultado.length).toBeGreaterThan(0);
-
-    resultado.forEach((reconhecimento) => {
-      expect(reconhecimento).toHaveProperty('id');
-      expect(reconhecimento).toHaveProperty('uuid');
-      expect(reconhecimento).toHaveProperty('from');
-      expect(reconhecimento).toHaveProperty('to');
-      expect(reconhecimento).toHaveProperty('message');
-      expect(reconhecimento).toHaveProperty('coins');
-    });
+    expect(resultado).toEqual([mockReconhecimento]);
+    expect(repo.find).toHaveBeenCalled();
   });
 
   it('deve retornar um reconhecimento com o ID fornecido', async () => {
     const resultado = await service.findById(1);
-
-    expect(resultado).toEqual({
-      uuid: "1",
-      id: 1,
-      from: "Fulano",
-      to: "Ciclano",
-      message: "Excelente trabalho na entrega do projeto, sua dedicação fez toda a diferença!",
-      coins: 10
-    });
+    expect(resultado).toEqual(mockReconhecimento);
+    expect(repo.findOneByOrFail).toHaveBeenCalledWith({ id: 1});
   });
 
   it('deve excluir um reconhecimento com o ID fornecido', async () => {
-    await service.remove(1);
-
-    const removido = await service.findById(1);
-
-    expect(removido).toBeUndefined();
-
+    const resultado = await service.remove(1);
+    expect(resultado).toEqual(mockReconhecimento);
+    expect(repo.remove).toHaveBeenCalledWith(mockReconhecimento);
   });
 });
